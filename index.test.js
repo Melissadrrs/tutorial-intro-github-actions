9c951fb09ef1692a9488668c92d262315c2e39bc
melissadrrs <melissadrrs@gmail.com>
2022-06-23 12:17:09 -0500

    test 1

--- index.test.js
+++ index.test.js
@@ -8,6 +8,7 @@ test("array with dupes should return smaller array", () => {
   expect(removeDupes([1, 2, 3, 3, 3])).toStrictEqual([1, 2, 3]);
 });
 
+
 test("array with no values should return empty array", () => {
   expect(removeDupes([])).toStrictEqual([]);
 });
